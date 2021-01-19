d261c5098565cfa53a95864f4092435605596943
Mira <mira927@gmail.com>
2021-01-18 23:00:00 -0600

    more tests and stuff, recipe book

--- fridge.py
+++ fridge.py
@@ -1,36 +1,68 @@
+import os
 import yaml
 
 class Ingredient(object):
 
-    def __init__(self, description, date_aquired, amount_left):
+    def __init__(self, description, date_aquired, amount):
         self.description = description
         self.date_aquired = date_aquired
-        self.amount_left = amount_left
+        self.amount = amount
+
+
+class RecipeBook(object):
+
+    def __init__(self, recipe_directory=None, associated_fridge_file=None):
+        self.recipes = []
+        if recipe_directory is not None:
+            self._build_recipes_from_disk(recipe_directory)
+        self.fridge = Fridge(associated_fridge_file)
+
+    def _build_recipes_from_disk(self, directory):
+        for _filename in os.listdir(directory): 
+            recipe = Recipe('%s/%s' % (directory, _filename))
+            self.recipes.append(recipe)
+
+    def get_possible_recipes(self):
+        possible_recipes = []
+        for recipe in self.recipes:
+            if self.fridge.can_make_recipe(recipe):
+                possible_recipes.append(recipe.description)
+        return possible_recipes
+
+    ## TODO : this is the main question the app solves haha
+    def get_optimal_recipe(self):
+        pass
+
 
 class Recipe(object):
 
-    def __init__(self, recipe_file):
+    def __init__(self, recipe_file=None):
         self.ingredients = []
-        self._init_from_file(recipe_file)
-
-    def _ingredients_from_yaml(self, ingredients):
-        pass
+        if recipe_file:
+            self._init_from_file(recipe_file)
 
     def _init_from_file(self, recipe_file):
         with open(recipe_file) as file:
             recipe_yaml = yaml.load(
                 file, Loader= yaml.FullLoader
             )
+            ingredients = recipe_yaml['ingredients']
+            for ingredient in ingredients:
+                self.ingredients.append(
+                    Ingredient(ingredient['name'], None, ingredient['amount'])
+                )
+
 
 class Fridge(object):
 
-    def __init__(self, fridge_file):
+    def __init__(self, fridge_file=None):
         self.ingredients = {}
-        self._init_from_file(fridge_file)
+        if fridge_file:
+            self._init_from_file(fridge_file)
 
     def _ingredient_from_yaml(self, _key, _yaml):
         return Ingredient(
-            _key, _yaml['date aquired'], _yaml['amount left']
+            _key, _yaml['date added'], _yaml['amount']
         )
 
     def _init_from_file(self, fridge_file):
@@ -42,6 +74,17 @@ class Fridge(object):
                 if _key not in self.ingredients:
                     self.ingredients[_key] = self._ingredient_from_yaml(_key, _yaml)
 
-    def get_missing_ingredients(self):
-        pass
+
+    def can_make_recipe(self, recipe):
+        for ingredient in recipe.ingredients:
+            if ingredient.description not in self.ingredients:
+                return False
+        return True
+
+    def get_missing_ingredients(self, recipe):
+        missing_ingredients = []
+        for ingredient in recipe.ingredients:
+            if ingredient.description not in self.ingredients:
+                missing_ingredients.append(ingredient.description)
+        return missing_ingredients
 
--- test.py
+++ test.py
@@ -1,16 +1,57 @@
 import unittest
-from fridge import Fridge, Ingredient, Recipe
+from fridge import Fridge, Ingredient, Recipe, RecipeBook
 
 class TestItemsAndFridge(unittest.TestCase):
 
     def setUp(self):
-        RECIPE_FILE='recipes/enchiladas.yaml'
-        FRIDGE_FILE='fridge.yaml'
-        self.recipe = Recipe(RECIPE_FILE)
-        self.fridge = Fridge(FRIDGE_FILE)
-
-    def test_one(self):
         pass
+
+    def test_can_make_recipes(self):
+
+        recipe = Recipe()
+        recipe.ingredients.append(Ingredient('peanut butter', None, '5 tablespoons'))
+        recipe.ingredients.append(Ingredient('bread', None, '2 slices'))
+        recipe.ingredients.append(Ingredient('jelly', None, '5 tablespoons'))
+
+        fridge = Fridge()
+        fridge.ingredients['peanut butter'] = Ingredient('peanut butter', None, '1 jar')
+        fridge.ingredients['bread'] = Ingredient('bread', None, '1 loaf')
+        self.assertFalse(fridge.can_make_recipe(recipe))
+
+        fridge.ingredients['jelly'] = Ingredient('jelly', None, '1 jar')
+        self.assertTrue(fridge.can_make_recipe(recipe))
+
+    def test_get_missing_ingredients(self):
+
+        recipe = Recipe()
+        recipe.ingredients.append(Ingredient('peanut butter', None, '5 tablespoons'))
+        recipe.ingredients.append(Ingredient('bread', None, '2 slices'))
+        recipe.ingredients.append(Ingredient('jelly', None, '5 tablespoons'))
+
+        fridge = Fridge()
+        missing_ingredients = fridge.get_missing_ingredients(recipe)
+        self.assertEqual(missing_ingredients, ['peanut butter', 'bread', 'jelly'])
+
+        fridge.ingredients['peanut butter'] = Ingredient('peanut butter', None, '1 jar')
+        missing_ingredients = fridge.get_missing_ingredients(recipe)
+        self.assertEqual(missing_ingredients, ['bread', 'jelly'])
+
+        fridge.ingredients['bread'] = Ingredient('bread', None, '1 loaf')
+        missing_ingredients = fridge.get_missing_ingredients(recipe)
+        self.assertEqual(missing_ingredients, ['jelly'])
+
+        fridge.ingredients['jelly'] = Ingredient('jelly', None, '1 jar')
+        missing_ingredients = fridge.get_missing_ingredients(recipe)
+        self.assertEqual(missing_ingredients, [])
+
+    def test_get_possible_recipes(self):
+
+        RECIPE_DIR="recipes"
+        FRIDGE_FILE="fridge.yaml"
+
+        recipe_book = RecipeBook(RECIPE_DIR, FRIDGE_FILE) 
+
+
 
 if __name__ == '__main__':
     unittest.main()
